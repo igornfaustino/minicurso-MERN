69ef0d2daaf39e48790900ac78fb7ef16982d3fa
claudiaps <clp.sampedro@gmail.com>
2018-08-27 14:12:39 -0300

    new functions in db

--- minicurso/back/models/todo.js
+++ minicurso/back/models/todo.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose")
 
 const todoSchema = mongoose.Schema({
-    listName: {type: String, require: true},
+    listName: { type: String, require: true },
     desc: String,
     todos: [
         {
-            name: {type: String, require: true},
+            name: { type: String, require: true },
             desc: String,
             checked: Boolean
         }
@@ -14,3 +14,26 @@ const todoSchema = mongoose.Schema({
 
 module.exports = mongoose.model("Todo", todoSchema)
 const Todo = mongoose.model("Todo", todoSchema)
+
+module.exports.getTodoById = (id, callback) => {
+    Todo.findOne({ _id: id }, callback)
+}
+
+module.exports.getAllTodos = (callback) => {
+    Todo.find({}, callback)
+}
+
+module.exports.addTodo = (todo, callback) => {
+    Todo.create(todo, callback)
+}
+
+module.exports.updateTodo = (id, updatedTodo, callback) => {
+    Todo.getTodoById(id, (err, todo) => {
+        if (err) {
+            callback(err, null)
+        }
+        todo.listName = updatedTodo.listName ? updatedTodo.listName : todo.listName
+        todo.desc = updatedTodo.desc ? updatedTodo.desc : todo.desc
+        todo.todos = updatedTodo.todos ? updatedTodo.todos : todo.todos
+    })
+}
